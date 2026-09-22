9db9155a02e24689ca6570adc09bbd48f9f9475d
w0fv1 <wofbi1@outlook.com>
2026-09-22 21:18:43 +0800

    chore: update Norm to 0.24.0

--- Formula/normlang.rb
+++ Formula/normlang.rb
@@ -1,22 +1,22 @@
 class Normlang < Formula
   desc "Statically typed programming language and toolchain"
   homepage "https://github.com/normlanguage/Norm"
-  version "0.23.2"
+  version "0.24.0"
   license "MPL-2.0"
 
   on_macos do
     depends_on arch: :arm64
     on_arm do
-      url "https://github.com/normlanguage/Norm/releases/download/v0.23.2/norm-v0.23.2-macos-arm64.tar.gz"
-      sha256 "3ed2855bf1f606147691dc8df0fcc4c22fdfc127b992dbf0b063b97f5246fed1"
+      url "https://github.com/normlanguage/Norm/releases/download/v0.24.0/norm-v0.24.0-macos-arm64.tar.gz"
+      sha256 "f14c86c9248b816a22eab3c6dd98c2d768589c82685fc8adcaad75fa70841622"
     end
   end
 
   on_linux do
     depends_on arch: :x86_64
     on_intel do
-      url "https://github.com/normlanguage/Norm/releases/download/v0.23.2/norm-v0.23.2-linux-x64.tar.gz"
-      sha256 "2465a590b40d2372ade8326219f8a0ee40d6c9454ff951044bf5a8891efbae0b"
+      url "https://github.com/normlanguage/Norm/releases/download/v0.24.0/norm-v0.24.0-linux-x64.tar.gz"
+      sha256 "ff66a5b055bad8648f11462887a57b42de59f0e43e967058ef8ebc767d64432a"
     end
   end
 
